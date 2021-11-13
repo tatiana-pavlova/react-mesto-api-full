d11bc4256e9186bb6be60c96b5eb73cff68729e7
Tatiana Pavlova <pavlova.tat.ole@gmail.com>
2021-11-13 14:41:42 +0300

    feat: add cors

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,34 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const allowedCors = [
+  'http://mesto.tatianapavlova.nomoredomains.rocks/',
+  'https://mesto.tatianapavlova.nomoredomains.rocks/',
+  'localhost:3000',
+];
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   autoIndex: true,
+});
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
 });
 
 app.use(express.json());
