c8cca8377e01f52a9c8050b803b7d021b418d113
Tatiana Pavlova <pavlova.tat.ole@gmail.com>
2021-11-14 21:48:41 +0300

    fix: correct cors mistakes

--- backend/app.js
+++ backend/app.js
@@ -32,9 +32,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  const { method } = req;
+  const { method } = req.method;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers['Access-Control-Request-Headers'];
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
